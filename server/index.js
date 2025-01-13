8a54181efc9ed7ffccd705afa7f14cad17518913
Oleksandra Pylypchuk <lemocream@gmail.com>
2025-01-13 23:50:19 +0100

    INIT

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import connectToDatabase from './db.js';
+import express from 'express';
+import cors from 'cors';
+
+connectToDatabase();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('api is running ...');
+});
+
+const port = 5000;
+app.listen(port, () => {
+	console.log('listening on port ' + port);
+});
