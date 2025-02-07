58aa6c1e770ed56f3c0db6c70624ef546c0ba530
Oleksandra Pylypchuk <lemocream@gmail.com>
2025-02-08 00:42:59 +0100

    add images

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import store from './redux/store';
+import store from './redux/store.js';
 import { Provider } from 'react-redux';
+import { Provider as ChakrapProvider } from '@/components/ui/provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<Provider strore={store}>
-			<App />
+		<Provider store={store}>
+			<ChakrapProvider>
+				<App />
+			</ChakrapProvider>
 		</Provider>
 	</React.StrictMode>
 );
--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,31 +1,25 @@
 import { Box, Flex, Center } from '@chakra-ui/react';
 import ProductCard from '../components/ProductCard';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getProducts } from '../redux/actions/productActions';
 
 const ProductsScreen = () => {
-	const [data, setData] = useState([]);
-
+	const dispatch = useDispatch();
+	const { loading, error, products, pagination } = useSelector((state) => state.product);
 	useEffect(() => {
-		axios
-			.get('/api/products')
-			.then((res) => {
-				setData(res.data.products);
-			})
-			.catch((err) => {
-				console.error('Error fetching data: ', err);
-			});
-	}, []);
+		dispatch(getProducts());
+	}, [dispatch]);
 
 	return (
 		<>
-			{data.length > 1 && (
+			{products.length > 1 && (
 				<Box>
 					<Flex wrap='wrap' spacing='30px' justify='center' minHeight='80vh' mx={{ base: '12', md: '20', lg: '32' }}>
-						{data.map((product) => (
+						{products.map((product) => (
 							<Box key={product._id}>
 								<Center w='250px' h='450px'>
-									<ProductCard product={product} loading={false} />
+									<ProductCard product={product} loading={loading} />
 								</Center>
 							</Box>
 						))}
