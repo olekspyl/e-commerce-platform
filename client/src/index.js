55de73e3ed4d5bbaa0a98924ec4a4c9f1d879e47
Oleksandra Pylypchuk <lemocream@gmail.com>
2025-02-17 00:52:07 +0100

    add header and favorite frontend

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import store from './redux/store.js';
 import { Provider } from 'react-redux';
-import { Provider as ChakrapProvider } from '@/components/ui/provider';
+import { Provider as ChakraProvider } from '@/components/ui/provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<ChakrapProvider>
+			<ChakraProvider>
 				<App />
-			</ChakrapProvider>
+			</ChakraProvider>
 		</Provider>
 	</React.StrictMode>
 );
