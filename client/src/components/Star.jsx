17a60f3354a52e362507ee8a393e9ecb1badc0b6
Oleksandra Pylypchuk <lemocream@gmail.com>
2025-03-18 13:34:23 +0100

    Ended ProductScreen

---
+++ client/src/components/Star.jsx
@@ -0,0 +1,7 @@
+import { StarIcon } from '@chakra-ui/icons';
+
+export const Star = ({ rating = 0, star = 0 }) => (
+	<StarIcon color={rating >= star || rating === 0 ? 'cyan.500' : 'grey.200'} />
+);
+
+export default Star;
