78f9cb83db008b31316b71e761e5d248df4f478c
Олександра <oleksandra@macbook-air-oleksandra.local>
2025-04-13 22:32:06 +0200

    add forgotten password form

---
+++ client/src/components/PasswordForgottenForm.jsx
@@ -0,0 +1,27 @@
+import {Text, Stack, Box, Button, Input} from "@chakra-ui/react"
+import {useState} from 'react'
+import {useDispatch} from 'react-redux'
+import {sendResetEmail} from '../redux/actions/userActions'
+
+
+const PasswordForgottenForm = () => {
+   const dispatch = useDispatch();
+   const [email, setEmail] = useState('');
+   const handleChange = (e) => {
+       setEmail(e.target.value)
+   }
+    return (
+        <>
+        <Box my='4'>
+            <Text as='b'>Enter your email address below. </Text>
+            <Text>We'll send you an email with a link to reset your password.</Text>
+        </Box>
+            <Stack>
+                <Input mb='4' type='text' name='email' placeholder='label' value={email} onChange={(e) => handleChange(e)}  />
+                <Button colorScheme='yellow' size='lg' fontSize='md' onClick={() => dispatch(sendResetEmail(email))}>Send Reset Email</Button>
+            </Stack>
+        </>
+    )
+}
+
+export default PasswordForgottenForm
