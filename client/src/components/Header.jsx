8cb3ce1ba73688d04afb46c3b6eb8246f959cb24
olekspyl <lemocream@gmail.com>
2025-04-22 09:09:32 +0200

    add header login logic

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
+import { ChevronDownIcon, CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
 import {
 	Box,
 	Flex,
@@ -9,9 +9,22 @@ import {
 	Text,
 	useColorModeValue as mode,
 	useDisclosure,
+	Alert,
+	AlertDescription,
+	AlertIcon,
+	AlertTitle,
+	Divider,
+	Image,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	Spacer,
+	useToast,
+	MenuDivider
 } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
-import { BiUserCheck } from 'react-icons/bi'
+import React, { useEffect, useState } from 'react'
+import { BiUserCheck, BiLogInCircle } from 'react-icons/bi'
 import { BsPhoneFlip } from 'react-icons/bs'
 import { MdOutlineFavorite, MdOutlineFavoriteBorder } from 'react-icons/md'
 import { TbShoppingCart } from 'react-icons/tb'
@@ -20,6 +33,8 @@ import { Link as ReactLink } from 'react-router-dom'
 import { toggleFavorites } from '../redux/actions/productActions'
 import ColorModeToggle from './ColorModeToggle.jsx'
 import NavLink from './NavLink'
+import { logout } from '../redux/actions/userActions.js'
+import {MdOutlineAdminPanelSettings} from 'react-icons/md'
 
 const Links = [
 	{ name: 'Products', route: '/products' },
@@ -31,10 +46,23 @@ const Links = [
 const Header = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure()
 	const dispatch = useDispatch()
+	const toast = useToast()
 	const { favoritesToggled } = useSelector(state => state.product)
 	const { cartItems } = useSelector(state => state.cart)
+	const {userInfo} = useSelector(state => state.user)
+	
 
 	useEffect(() => {}, [favoritesToggled, dispatch])
+	
+	const logoutHandler = () => {
+		dispatch(logout())
+		toast({
+			description: 'Logout successful',
+			status: 'success',
+			isClosable: true,
+		})
+	}
+	
 	return (
 		<Box bg={mode('cyan.300', 'gray.900')} px='4'>
 			<Flex h='16' alignItems='center' justifyContent='space-between'>
@@ -120,7 +148,50 @@ const Header = () => {
 					</HStack>
 				</HStack>
 				<Flex alignItems='center'>
-					<BiUserCheck />
+					{userInfo ?
+						(
+							<Menu>
+								<MenuButton
+									rounded='full'
+									minW='0'
+										variant='link'
+									cursor='pointer'
+								>
+									<HStack>
+										<BiUserCheck size='30'/>
+										<ChevronDownIcon />
+									</HStack>
+								</MenuButton>
+								<MenuList>
+									<HStack>
+										<Text pl='3' as='i'>
+											{userInfo.email}
+										</Text>
+									</HStack>
+									<Divider py='1'/>
+									<MenuItem as={ReactLink} to='/order-history'>Order History</MenuItem>
+									<MenuItem as={ReactLink} to='/profile'>Profile</MenuItem>
+									{userInfo.isAdmin && (
+										<>
+											<MenuDivider/>
+											<MenuItem as='ReactLink' to='/profile'>Profile</MenuItem>
+										</>
+									)}
+									<MenuDivider/>
+									<MenuItem onClick={logoutHandler}>Logout</MenuItem>
+								</MenuList>
+							</Menu>
+						)
+						: (
+							<Menu>
+								<MenuButton as={IconButton} variant='ghost' cursor='pointer' icon={<BiLogInCircle size='25px'/> }/>
+								<MenuList>
+									<MenuItem as={ReactLink} to='/login' p='2' fontWeight='400' variant='link'>Sign in</MenuItem>
+									<MenuDivider/>
+									<MenuItem as={ReactLink} to='/registration' p='2' fontWeight='400' variant='link'>Sign up</MenuItem>
+									</MenuList>
+							</Menu>
+						)}
 				</Flex>
 			</Flex>
 			<Box display='flex'>
