8d4bce9988363bf017339565964c3825897af612
olekspyl <lemocream@gmail.com>
2025-05-26 08:03:56 +0200

    updated craco config

--- client/craco.config.js
+++ client/craco.config.js
@@ -4,9 +4,26 @@ module.exports = {
 	webpack: {
 		alias: {
 			'@': path.resolve(__dirname, 'src')
+		},
+		configure: (webpackConfig, { env, paths }) => {
+			// Знаходимо babel-loader
+			const babelLoader = webpackConfig.module.rules
+				.find(rule => Array.isArray(rule.oneOf)).oneOf
+				.find(rule => rule.loader && rule.loader.includes('babel-loader'))
+			
+			if (babelLoader) {
+				// Фільтруємо плагіни, якщо продакшн
+				babelLoader.options.plugins = babelLoader.options.plugins.filter(
+					plugin =>
+						!(
+							typeof plugin === 'string' && plugin.includes('react-refresh'))
+				)
+			}
+			
+			return webpackConfig
 		}
 	},
 	resolve: {
-		extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'] // Додаємо підтримку TS і JSX
+		extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
 	}
 }
