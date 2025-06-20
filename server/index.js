711b6132160d45635728681fbb878a6c9ec1b2b9
olekspyl <lemocream@gmail.com>
2025-06-20 14:25:42 +0200

    updated

--- server/index.js
+++ server/index.js
@@ -38,10 +38,11 @@ const port = process.env.PORT || 5001
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client', 'build')))
+	const clientBuildPath = path.resolve(__dirname, '../client/build')
+	app.use(express.static(clientBuildPath))
 	
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.join(clientBuildPath, 'index.html'))
 	})
 }
 
